c2dab361b9c997c22805afbaec35f8cfe4bcd123
Joao Guilherme <jgpp@cin.ufpe.br>
2023-06-07 17:41:40 -0300

    teste de requisicao

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,9 +3,10 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
-  webDir: 'www',
+  webDir: './www',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'http',
+    cleartext: true,
   }
 };
 
